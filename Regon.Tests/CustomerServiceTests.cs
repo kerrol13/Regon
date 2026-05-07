ae9bb1694e9dcbc7e08c4707adbc26552cff07b1
Kane Errol <Productivity@DESKTOP-RETQBNT>
2026-05-08 02:01:32 +0800

    Deleted images file and updated the unittest

--- Regon.Tests/CustomerServiceTests.cs
+++ Regon.Tests/CustomerServiceTests.cs
@@ -1,4 +1,7 @@
-﻿using Moq;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Internal;
+using Moq;
+using Regon.Application.Common;
 using Regon.Application.Dtos.Customer;
 using Regon.Application.Services;
 using Regon.Domain.Entities;
@@ -18,17 +21,27 @@ namespace Regon.Tests
         {
             var repoMock = new Mock<ICustomerRepository>();
 
-            var service = new CustomerService(repoMock.Object);
+            var localFileServiceMock = new Mock<ILocalFileService>();
+
+            localFileServiceMock
+            .Setup(x => x.SaveFileAsync(It.IsAny<IFormFile>()))
+            .ReturnsAsync("fake-signature.png");
+
+            var service = new CustomerService(repoMock.Object, localFileServiceMock.Object);
+
+            var fileMock = new Mock<IFormFile>();
 
             var customer = new CustomerCreateDto
             {
                 FirstName = "Sample",
                 LastName = "Test",
-                Email = "sample@test.com"
+                Email = "sample@test.com",
+                Signature = CreateFakeFile(),
+                PhoneNumber = "1234567890"
             };
 
-            await service.AddAsync(customer, "signature-path");
-
+            await service.AddAsync(customer);
+            localFileServiceMock.Verify(r => r.SaveFileAsync(It.IsAny<IFormFile>()), Times.Once);
             repoMock.Verify(r => r.AddAsync(It.IsAny<Customer>()), Times.Once);
             repoMock.Verify(r => r.SaveChangesAsync(), Times.Once);
         }
@@ -37,6 +50,7 @@ namespace Regon.Tests
         public async Task GetCustomerById_Should_ReturnCustomer()
         {
             var repoMock = new Mock<ICustomerRepository>();
+            var localFileServiceMock = new Mock<ILocalFileService>();
 
             var expectedCustomer = new Customer
             {
@@ -47,7 +61,7 @@ namespace Regon.Tests
             repoMock.Setup(r => r.GetByIdAsync(expectedCustomer.Id))
                     .ReturnsAsync(expectedCustomer);
 
-            var service = new CustomerService(repoMock.Object);
+            var service = new CustomerService(repoMock.Object, localFileServiceMock.Object);
 
             var result = await service.GetByIdAsync(expectedCustomer.Id);
 
@@ -59,6 +73,7 @@ namespace Regon.Tests
         public async Task GetAllCustomers_ShouldReturn_AllCustomers()
         {
             var repoMock = new Mock<ICustomerRepository>();
+            var localFileServiceMock = new Mock<ILocalFileService>();
 
             var expectedCustomers = new List<Customer>
             {
@@ -70,7 +85,7 @@ namespace Regon.Tests
                 .Setup(r => r.GetAllAsync())
                 .ReturnsAsync(expectedCustomers);
 
-            var service = new CustomerService(repoMock.Object);
+            var service = new CustomerService(repoMock.Object, localFileServiceMock.Object);
 
             var result = await service.GetAllAsync();
 
@@ -78,6 +93,14 @@ namespace Regon.Tests
             Assert.Equal(2, result.Count());
 
             repoMock.Verify(r => r.GetAllAsync(), Times.Once);
+        }
+
+        private IFormFile CreateFakeFile()
+        {
+            var content = "fake file content";
+            var stream = new MemoryStream(System.Text.Encoding.UTF8.GetBytes(content));
+
+            return new FormFile(stream, 0, stream.Length, "Signature", "sig.png");
         }
     }
 }
